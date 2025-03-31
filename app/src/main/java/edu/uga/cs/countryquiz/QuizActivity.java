e321e1103c649321ee303b74cf87b54704c16de8
Yen Vo <yenhvo2004@gmail.com>
2025-03-31 13:09:03 -0400

    Added onQuizCreated() basics and modified fragment handling

--- app/src/main/java/edu/uga/cs/countryquiz/QuizActivity.java
+++ app/src/main/java/edu/uga/cs/countryquiz/QuizActivity.java
@@ -10,9 +10,12 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.uga.cs.countryquiz.models.Country;
+import edu.uga.cs.countryquiz.models.Question;
 import edu.uga.cs.countryquiz.models.Quiz;
 
 public class QuizActivity extends AppCompatActivity {
@@ -38,4 +41,41 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     // todo: can make the quiz here and pass the information to the fragment
+    // todo: ensure that the country schema matches Country class in which it can use getContinent()
+    // todo: optional but modify the Question and Quiz class so that its easier to add questions to the class without needing to create a new arraylist everytime?
+
+    public void onQuizCreated() {
+        List<Question> quizQuestions = new ArrayList<Question>();
+        for (int i = 0; i < 6; i++) {
+            List<Country> options = new ArrayList<Country>();
+            Country selectedCountry = new Country();
+            // Selecting a country from the database
+            // todo: find a way to pull a random country from the database
+
+            // Selecting other random continents from the database
+            // todo: add the other continents into the list
+
+            // Randomizing the options
+            // todo: finding a way to randomize the options
+
+            Question question = new Question(selectedCountry, options);
+            quizQuestions.add(question);
+        }
+
+        Quiz quiz = new Quiz(quizQuestions);
+        // todo: pass the quiz to the fragment
+
+        QuestionFragment questionFragment = new QuestionFragment();
+        Bundle args = new Bundle();
+        // todo: find a way to pass quiz into args. maybe: args.putSerializable("quiz", (Serializable) quiz);
+        questionFragment.setArguments(args);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, questionFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        // todo: double check this code with his own, not 100%
+    }
 }
--- app/src/main/java/edu/uga/cs/countryquiz/models/Question.java
+++ app/src/main/java/edu/uga/cs/countryquiz/models/Question.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Question {
     private final Country country;
-    private final List<String> options;
+    private final List<Country> options;
     private int selectedAnswer; // default for unselected
 
-    public Question(Country country, List<String> options) {
+    public Question(Country country, List<Country> options) {
         this.country = country;
         this.options = options;
         this.selectedAnswer = -1;
@@ -17,7 +17,7 @@ public class Question {
         return country;
     }
 
-    public List<String> getOptions() {
+    public List<Country> getOptions() {
         return options;
     }
 
